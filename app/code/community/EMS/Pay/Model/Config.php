db1113f94d6c8bc7e78d6ace17c920bfa1c65e87
algol1980 <algol1980@uandex.ru>
2018-05-07 14:01:08 +0300

    Implement iDeal Customer Id field input

--- app/code/community/EMS/Pay/Model/Config.php
+++ app/code/community/EMS/Pay/Model/Config.php
@@ -50,6 +50,7 @@ class EMS_Pay_Model_Config
     const XML_CONFIG_SHARED_SECRET_PRODUCTION = 'payment/ems_pay_general/shared_secret_production';
     const XML_CONFIG_LOGGING_ENABLED = 'payment/ems_pay_general/log_enabled';
     const XML_CONFIG_IDEAL_BANK_SELECTION = 'payment/ems_pay_ideal/bank_selection_enabled';
+    const XML_CONFIG_IDEAL_CUSTOMER_ID_SELECTION = 'payment/ems_pay_ideal/customerid_selection_enabled';
     const XML_CONFIG_BANCONTACT_BANK_SELECTION = 'payment/ems_pay_bancontact/bank_selection_enabled';
     const XML_CONFIG_CC_TYPES = 'payment/ems_pay_cc/cctypes';
     const XML_CONFIG_CC_3DSECURE = 'payment/ems_pay_cc/enable_3dsecure';
@@ -382,6 +383,14 @@ class EMS_Pay_Model_Config
     public function isIdealIssuingBankSelectionEnabled()
     {
         return Mage::getStoreConfigFlag(self::XML_CONFIG_IDEAL_BANK_SELECTION);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isIdealCustomerIdSelectionEnabled()
+    {
+        return Mage::getStoreConfigFlag(self::XML_CONFIG_IDEAL_CUSTOMER_ID_SELECTION);
     }
 
 
